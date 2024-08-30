0ad5358f2dcc74b26a942ddd93906d9769bf0795
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-08-30 11:01:07 +0200

    Modified and implemented class

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -24,10 +24,26 @@ public class App {
                         LocalDate.of(2024, 2, 10), false, person1);
 
         TodoItemTask todoItemTask1 = new TodoItemTask(todoItem1, person1);
-        String todoItemTaskDetails = todoItemTask1.getSummary();
-        System.out.println(todoItemTaskDetails);
+        String todoItemTaskDetails1 = todoItemTask1.getSummary();
+        System.out.println(todoItemTaskDetails1);
 
         System.out.println();
+
+        TodoItem todoItem2 = new TodoItem("Check oil", "Check brakes",
+                        LocalDate.of(2024, 4, 2), false, person2);
+
+        TodoItemTask todoItemTask2 = new TodoItemTask(todoItem2, person2);
+        String todoItemTaskDetails2 = todoItemTask2.getSummary();
+        System.out.println(todoItemTaskDetails2);
+
+        System.out.println();
+
+        TodoItemTask todoItemTask3 = new TodoItemTask(todoItem2, person3);
+        String todoItemTaskDetails3 = todoItemTask3.getSummary();
+        System.out.println(todoItemTaskDetails3);
+
+        System.out.println();
+
 
 
 
