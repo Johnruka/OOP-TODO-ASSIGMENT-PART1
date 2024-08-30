d72243b42e73d017db70e9158c672758b568534a
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-08-30 10:09:58 +0200

    Modified and implemented class

--- src/main/java/se/lexicon/TodoItemTask.java
+++ src/main/java/se/lexicon/TodoItemTask.java
@@ -1,4 +1,53 @@
 package se.lexicon;
 
 public class TodoItemTask {
+
+    private int id;
+    private boolean assigned;
+    private TodoItem todoItem;
+    private Person assignee;
+
+    public TodoItemTask(TodoItem todoItem, Person assignee) {
+        setTodoItem(todoItem);
+        setAssignee(assignee);
+    }
+
+    public TodoItemTask(TodoItem todoItem) {
+        setTodoItem(todoItem);
+    }
+
+    public TodoItemTask(int id, TodoItem todoItem, Person assignee) {
+        this(todoItem, assignee);
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isAssigned() {
+        return assigned;
+    }
+
+    public TodoItem getTodoItem() {
+        return todoItem;
+    }
+
+    public void setTodoItem(TodoItem todoItem) {
+        if (todoItem == null) throw new IllegalArgumentException("todoItem is null");
+        this.todoItem = todoItem;
+    }
+
+    public Person getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(Person assignee) {
+        this.assignee = assignee;
+        this.assigned = (this.assignee != null);
+    }
 }
